d3b375beb7c90128de287f2f3678070672a6bf92
munis36 <munis36@hotmail.com>
2018-01-31 16:53:19 -0500

    new commit

---
+++ src/main/java/com/munis/javafx/MyController.java
@@ -0,0 +1,19 @@
+package com.munis.javafx;
+
+import javafx.application.Application;
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by MN883H on 1/31/2018.
+ */
+public class MyController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
